448cbfe677d5b2b44404a463697abc1bedacc89c
UltraDEV007 <LeTuan2024@outlook.com>
2020-09-03 10:44:39 +0900

    Merge branch 'dev' into 4.0
    # Conflicts:
    #	package.json
    #	yarn.lock

---
+++ src/index.cjs.js
@@ -0,0 +1,17 @@
+import { createStore, Store } from './store'
+import { useStore } from './injectKey'
+import { mapState, mapMutations, mapGetters, mapActions, createNamespacedHelpers } from './helpers'
+import { createLogger } from './plugins/logger'
+
+export default {
+  version: '__VERSION__',
+  createStore,
+  Store,
+  useStore,
+  mapState,
+  mapMutations,
+  mapGetters,
+  mapActions,
+  createNamespacedHelpers,
+  createLogger
+}
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import { createStore, Store } from './store'
+import { useStore } from './injectKey'
+import { mapState, mapMutations, mapGetters, mapActions, createNamespacedHelpers } from './helpers'
+import { createLogger } from './plugins/logger'
+
+export default {
+  version: '__VERSION__',
+  createStore,
+  Store,
+  useStore,
+  mapState,
+  mapMutations,
+  mapGetters,
+  mapActions,
+  createNamespacedHelpers,
+  createLogger
+}
+
+export {
+  createStore,
+  Store,
+  useStore,
+  mapState,
+  mapMutations,
+  mapGetters,
+  mapActions,
+  createNamespacedHelpers,
+  createLogger
+}
